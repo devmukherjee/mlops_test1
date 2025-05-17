7f18adb338379338d9ceb51357fe2762ff009c69
Dev Mukherjee <devmukherjeeindia@gmail.com>
2025-05-17 20:29:49 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.chicken_disease_classification import logger
 from src.chicken_disease_classification.pipeline.stage_01_data_ingestion import DataIngestionPipeline
-
+from src.chicken_disease_classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 STAGE_NAME= "01 Data Ingestion Stage"
 try:
     pass
@@ -12,3 +12,14 @@ except Exception as e:
     logger.exception(e)
     raise e
 logger.info("Logging has been set up successfully")
+
+STAGE_NAME= "Prepare base model"
+try:
+        pass
+        logger.info(f"*************** Stage: {STAGE_NAME} started <<<<<<<<<<<<<<<<<<<<<<<<<<")
+        obj= PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f"*************** Stage: {STAGE_NAME} completed <<<<<<<<<<<<<<<<<<<<<<<<<<")
+except Exception as e:
+        logger.exception(e)
+        raise e
